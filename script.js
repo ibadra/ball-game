c53f41026f0d49547802618c47b2c8b0e289a776
Ivon Badra <ibadra.c@gmail.com>
2021-08-30 15:46:02 -0400

    Bouncing ball

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 const canvas = document.getElementById("gameArea");
 const ctx = canvas.getContext("2d");
-window.addEventListener("resize", resizeCanvas, false);
+// window.addEventListener("resize", resizeCanvas, false);
 
-function resizeCanvas() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+// function resizeCanvas() {
+//   canvas.width = window.innerWidth;
+//   canvas.height = window.innerHeight;
 
-  /**
-   * Your drawings need to be inside this function otherwise they will be reset when
-   * you resize the browser window and the canvas goes will be cleared.
-   */
-  drawStuff();
-}
+//   /**
+//    * The drawings need to be inside this function otherwise they will be reset when
+//    * you resize the browser window and the canvas goes will be cleared.
+//    */
+drawCanvas();
+// }
 
-resizeCanvas();
+// resizeCanvas();
 
-function drawStuff() {
-  // do your drawing stuff here
+function drawCanvas() {
+  //Variables
+  var x = canvas.width / 2;
+  var y = canvas.height - 30;
+  var dx = -6; //x motion direction
+  var dy = -2; // y morion direction
+  ballRadius = 20; //Used for collision detection
+
+  function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  function ballLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+    if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
+      dy = -dy;
+    } else if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
+      dx = -dx;
+    }
+  }
+  setInterval(ballLoop, 20);
 }
