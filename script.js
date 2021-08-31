8bc62ed24eb7a4b48dcda3d2253f53873db2468f
Ivon Badra <ibadra.c@gmail.com>
2021-08-31 00:32:15 -0400

    Brick collision detection

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = -6; //x motion direction
 let dy = -2; // y motion direction
-const ballRadius = 20; //Used for collision detection
+const ballRadius = 15; //Used for collision detection
 const paddleHeight = 10;
 const paddleWidth = 75;
 let paddleX = (canvas.width - paddleWidth) / 2;
 let rightMovement = false; //Right arrow is pressed
 let leftMovement = false; //Left arrow is pressed
 //Variables for bricks
-const brickRow = 3;
-const brickColumn = 5;
+const brickRowCount = 6;
+const brickColumnCount = 6;
 const brickWidth = 75;
 const brickHeight = 20;
-const brickPadding = 10;
-const brickOffsetTop = 30;
-const brickOffsetLeft = 30;
+const brickPadding = 3;
+const brickOffsetTop = 15;
+const brickOffsetLeft = 15;
 /////////////////////////////////////////////
 
 ///////////////////////EVENT LISTENERS/////////////////////
@@ -101,16 +101,70 @@ function drawPaddle() {
   ctx.closePath();
 }
 
+//BRICKS
+
+//Creating the bricks as a two-dimensional array using a for loop
+const bricks = [];
+for (let c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for (let r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
+  }
+}
+
+//Drawing the bricks in the canvas
+function drawBricks() {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      if (bricks[c][r].status === 1) {
+        const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+        const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+  }
+}
+
+//Collision detection for the bricks
+
+function collisionDetection() {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      let b = bricks[c][r];
+      if (b.status === 1) {
+        if (
+          x > b.x &&
+          x < b.x + brickWidth &&
+          y > b.y &&
+          y < b.y + brickHeight
+        ) {
+          dy = -dy;
+          dx = -dx;
+          b.status = 0;
+        }
+      }
+    }
+  }
+}
+
 function ballLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
   drawPaddle();
+  drawBricks();
+  collisionDetection();
   x += dx;
   y += dy;
   //Collision detection for Y axis
   if (y + dy < ballRadius) {
     dy = -dy;
-  } else if (y + dy > canvas.height + 2 * ballRadius) {
+  } else if (y + dy > canvas.height + ballRadius + 6) {
     alert("Game Over");
     document.location.reload();
     clearInterval(interval);
